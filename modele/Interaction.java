34926e5abf1800eaa1b84d2e02471985b0d560d1
Theotaquet <to.constant@gmail.com>
2016-12-12 12:34:04 +0100

    inc -> var & ineq -> contr

--- modele/Interaction.java
+++ modele/Interaction.java
@@ -28,50 +28,50 @@ public class Interaction {
 	 * Demande des données à l'utilisateur.
 	 */
 	public void demanderInfos() {
-		int incNb = 0, ineqNb = 0;
+		int varNb = 0, contrNb = 0;
 		Scanner scan = new Scanner(System.in);
 		String tmpCoeff;
 
-		//test sur le nombre d'inconnues
-		while(incNb<1) {
-			System.out.print("Entrez le nombre d'inconnues: ");
+		//test sur le nombre de variables
+		while(varNb<1) {
+			System.out.print("Entrez le nombre de contraintes: ");
 			try {
-				incNb = scan.nextInt();
+				varNb = scan.nextInt();
 			}
 			catch(Exception e) {
-				incNb = -1;
+				varNb = -1;
 				scan.next();
 			}
-			if(incNb<1)
-				System.out.println("Nombre d'inconnues incorrect.");
+			if(varNb<1)
+				System.out.println("Nombre de variables incorrect.");
 		}
 
-		//test sur le nombre d'inéquations
-		while(ineqNb<1) {
-			System.out.print("Entrez le nombre d'inéquations: ");
+		//test sur le nombre de contraintes
+		while(contrNb<1) {
+			System.out.print("Entrez le nombre de contraintes: ");
 			try {
-				ineqNb = scan.nextInt();
+				contrNb = scan.nextInt();
 			}
 			catch(Exception e) {
-				ineqNb = -1;
+				contrNb = -1;
 				scan.next();
 			}
-			if(ineqNb<1)
-				System.out.println("Nombre d'inéquations incorrect.");
+			if(contrNb<1)
+				System.out.println("Nombre de contraintes incorrect.");
 		}
-		//liste temporaire à laquelle on ajoute les inéquations ainsi que la fonction objectif un à un
+		//liste temporaire à laquelle on ajoute les contraintes ainsi que la fonction objectif un à un
 		List<Double> listeCoeff = new ArrayList<Double>();
 
 		//ajout de la fonction objectif
 		listeCoeff.clear();
-		System.out.println("Entrez les " + incNb + " coefficients des inconnues de la fonction objectif");
+		System.out.println("Entrez les " + varNb + " coefficients des variables de la fonction objectif");
 		System.out.println("Exemple: 1,4,3.5");
-		if(incNb==1)
+		if(varNb==1)
 			tmpCoeff = "1,4";
 		else
 			tmpCoeff = "";
 			
-		while(tmpCoeff.split(",").length!=incNb) {
+		while(tmpCoeff.split(",").length!=varNb) {
 			try {
 				System.out.print("Coefficients de la fonction objectif: ");
 				tmpCoeff = scan.next();
@@ -80,19 +80,19 @@ public class Interaction {
 				System.out.println("Fonction objectif entrée incorrectement.");
 			}
 			if(tmpCoeff.split(",").length!=incNb)
-				System.out.println("Le nombre de termes insérés ("+tmpCoeff.split(",").length+") n'est pas égal à celui attendu ("+incNb+").");
+				System.out.println("Le nombre de termes insérés ("+tmpCoeff.split(",").length+") n'est pas égal à celui attendu ("+varNb+").");
 		}
 
-		for(int i=0;i<incNb;i++) {
+		for(int i=0;i<varNb;i++) {
 			listeCoeff.add(Double.parseDouble(tmpCoeff.split(",")[i]));
 		}
 		//sauvegarde des coefficients de la fonction objectif pour l'ajouter plus tard dans la matrice
 		List<Double> objectif = new ArrayList<Double>(listeCoeff);
 
-		System.out.println("Entrez les " + incNb + " coefficients des variables suivis du terme indépendant des contraintes, séparés par des virgules.");
+		System.out.println("Entrez les " + varNb + " coefficients des variables suivis du terme indépendant des contraintes, séparés par des virgules.");
 		System.out.println("Exemple: 2,7,-3,18");
 
-		//ajout des coefficients pour chaque inéquation
+		//ajout des coefficients pour chaque contrainte
 		for(int i=0;i<ineqNb;i++) {
 			tmpCoeff="";
 			listeCoeff.clear();
@@ -109,7 +109,7 @@ public class Interaction {
 			this.matrice.ajouterLigne(new ArrayList<Double>(listeCoeff));
 		}
 
-		//ajout de la fonction objectif dans la matrice à l'aide de la liste créée précédemment.
+		//ajout de la fonction objectif dans la matrice à l'aide de la liste créée précédemment
 		this.matrice.ajouterLigne(new ArrayList<Double>(objectif));
 		scan.close();
 	}
