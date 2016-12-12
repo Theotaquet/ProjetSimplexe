4cad716dfeb9bbe8195ee7a181353ec28409f86c
Theotaquet <to.constant@gmail.com>
2016-12-12 11:49:17 +0100

    Update and rename RapportsIncorrectsException.java to exception/RapportsIncorrectsException.java

---
+++ exception/RapportsIncorrectsException.java
@@ -0,0 +1,22 @@
+package exception;
+
+/**
+ * L'exception RapportsIncorrectesException gère les erreurs rencontrées lors d'une division par 0,
+ * mais aussi lorsque que l'un des deux termes est négatif.
+ *
+ * @author Nicolas Verhaeghe
+ * @author Théo Constant
+ * @author Florian Vangaeveren
+ */
+public class RapportsIncorrectsException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Renvoie le message de l'exception.
+	 */
+	@Override
+	public String getMessage() {
+		return "Tous les rapports permettant de trouver la ligne du pivot sont négatifs. Il n'y a donc pas de solution possible pour ce problème.";
+	}
+}
